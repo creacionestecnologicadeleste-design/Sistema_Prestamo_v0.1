d1c21dd9fcddc6b127288d3ed4cce75d7b6483f2
Dadminete <empresatecnologicadeleste@gmail.com>
2026-02-25 18:50:10 -0400

    Inicio

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,13 @@ const nextConfig = {
     return [
       {
         source: "/dashboard",
-        destination: "/dashboard/default",
+        destination: "/dashboard/crm",
         permanent: false,
       },
     ];
+  },
+  experimental: {
+    allowedDevOrigins: ["http://172.16.0.25:3000", "http://localhost:3000"],
   },
 };
 
